082f03603c708c1eb8622c80e21db859bcc4e6a9
Vivek Halder <23259597-VivekHalder@users.noreply.replit.com>
2024-04-27 06:42:20 +0000

    add alternative PORT

--- server/constant.js
+++ server/constant.js
@@ -1,5 +1,4 @@
 const DB_NAME = "vivek";
-const PORT = 2100;
 const LIMIT = "16KB";
 
-export { DB_NAME, PORT, LIMIT };
+export { DB_NAME, LIMIT };
--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import connectDB from "./db/index.js";
-import { PORT } from "./constant.js";
 import { app } from "./app.js";
+
+const PORT = process.env.PORT || 8000;
 
 connectDB()
   .then(() => {
